4ff3651d12c7e6fe81f182f63e7343887525a603
Abdul Fatah <fatah03111995@gmail.com>
2024-04-16 11:46:04 +0700

    feat : styling appBar, adding button with list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,15 +5,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Color> colorButton = [
+      Colors.red,
+      Colors.orange,
+      Colors.yellow,
+      Colors.green,
+      Colors.blue,
+      Colors.purple
+    ];
+    List<Widget> button() {
+      List<Widget> buttons = [];
+      for (int i = 0; i < colorButton.length; i++) {
+        buttons.add(TextButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(colorButton[i]),
+          ),
+          child: const Text(''),
+        ));
+      }
+      return buttons;
+    }
+
     return Scaffold(
       appBar: AppBar(
-          centerTitle: true,
-          title: Text('Xylophone',
-              style: TextStyle(
-                  fontFamily: 'Poppins',
-                  color: Colors.amber[600],
-                  fontWeight: FontWeight.w500,
-                  letterSpacing: 1.5))),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen[200],
+        title: Text(
+          'Xylophone',
+          style: TextStyle(
+              fontFamily: 'Poppins',
+              color: Colors.amber[600],
+              fontWeight: FontWeight.w500,
+              letterSpacing: 1.5),
+        ),
+      ),
+      body: Row(children: button()),
     );
   }
 }
